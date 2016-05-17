25c1d9357a7fc412f48c011b570b13806418ce80
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-17 10:59:03 -0700

    added a search method that gets the results of a specific query to the Tunes and Takeout API

--- lib/tunes_takeout.rb
+++ lib/tunes_takeout.rb
@@ -1,6 +1,10 @@
 require 'httparty'
 
 module TunesTakeoutWrapper
-  # BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
+  BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
+
+  def self.search(query, limit = 10, seed = query)
+    HTTParty.get(BASE_URL + "v1/suggestions/search?query=#{query}&limit=#{limit}&seed=#{query}")
+  end
 
 end
