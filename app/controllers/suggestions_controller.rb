764349aa693e6327eaa487535cbe419579e6df19
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-17 16:58:13 -0700

    added @suggestions to #index by calling the search method on TunesTakeoutWrapper

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,9 @@
+require 'tunes_takeout'
+
 class SuggestionsController < ApplicationController
+  extend TunesTakeoutWrapper
+
+  def index
+    @suggestions = TunesTakeoutWrapper.search(params[:query])
+  end
 end
