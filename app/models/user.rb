aecc78fc9f678e1c53f7079164827b153aa1a089
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-17 15:58:03 -0700

    added create and destroy methods in the session.  in user.rb, it will either find or create a new instance of user based on the credentials from the spotify login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,24 @@ class User < ActiveRecord::Base
   validates :provider, presence: true
   validates :uid, presence: true
 
+  def self.find_or_create_from_omniauth(auth_hash)
+    # Find or create a user
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+
+    if !user.nil?
+      return user
+    else
+      user = User.new
+      user.uid = auth_hash["uid"]
+      user.provider = auth_hash["provider"]
+      # user.name = auth_hash["info"]["name"]
+      # user.email = auth_hash["info"]["email"]
+      
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end
 end
