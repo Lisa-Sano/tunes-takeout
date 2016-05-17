0dff62d0dc4bf53eb49224a8f35d8e3c8892d5f7
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-17 16:59:21 -0700

    added a module TunesTakeoutWrapper that interfaces with the Tunes and Takeout web API.

--- lib/tunes_takeout.rb
+++ lib/tunes_takeout.rb
@@ -3,8 +3,14 @@ require 'httparty'
 module TunesTakeoutWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
 
+  # suggestions = TunesTakeoutWrapper.search("avocado")["suggestions"]
+  # find a suggestion based on a query and optional limit
   def self.search(query, limit = 10, seed = query)
-    HTTParty.get(BASE_URL + "v1/suggestions/search?query=#{query}&limit=#{limit}&seed=#{query}")
+    HTTParty.get(BASE_URL + "v1/suggestions/search?query=#{query}&limit=#{limit}&seed=#{query}").parsed_response
   end
 
+  # retrieve a suggestion
+  def self.retrieve(id)
+    HTTParty.get(BASE_URL + "/v1/suggestions/#{id}").parsed_response
+  end
 end
