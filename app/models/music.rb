b70e3831e33d0fe00fcaf4f11ce8f6885baa7e69
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-18 09:54:34 -0700

    added a self.find method that finds an artist, track, album, or playlist based on music_id and music_type returned by the tunes and takeout web api

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,5 +1,16 @@
 require 'rspotify'
 
 class Music
-
+  
+  def self.find(id, type, user_id = nil)
+    if type == "artist"
+      RSpotify::Artist.find(id)
+    elsif type == "album"
+      RSpotify::Album.find(id)
+    elsif type == "track"
+      RSpotify::Track.find(id)
+    elsif type == "playlist"
+      RSpotify::Playlist.find(user_id, id)
+    end
+  end
 end
