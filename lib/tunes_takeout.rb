4b65365fb1d9a4ae5add1ad3ce11521d372b506b
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-19 16:20:18 -0700

    fixed a bug in the unfavorite method

--- lib/tunes_takeout.rb
+++ lib/tunes_takeout.rb
@@ -31,7 +31,7 @@ module TunesTakeoutWrapper
   end
 
   def self.unfavorite(user_id, suggestion_id)
-    HTTParty.post(BASE_URL + "/v1/users/#{user_id}/favorites", 
+    HTTParty.delete(BASE_URL + "/v1/users/#{user_id}/favorites", 
       :body => { "suggestion": "#{suggestion_id}" }.to_json,
       :headers => { 'Content-Type' => 'application/json' } )
   end
