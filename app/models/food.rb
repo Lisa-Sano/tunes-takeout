3641ee099df335af76c56be77d6381955abfd3cc
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-18 22:52:42 -0700

    added initialize method to food class. self.business now returns instances of Food with attributes name, url, rating, address, phone, and image_url. partial to show an individual suggestion is complete and index calls it while looping through the array of suggestions

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,11 +2,22 @@ require 'yelp'
 
 class Food
   BASE_URL = "https://api.yelp.com/"
+  attr_reader :name, :url, :rating, :address, :phone, :image_url
+
+  def initialize(data)
+    @name = data.business.name
+    @url = data.business.url
+    @rating = data.business.rating_img_url_large
+    @address = data.business.location.display_address
+    @phone = data.business.display_phone
+    @image_url = data.business.image_url
+  end
 
   # yelp business API
   def self.business(suggestions_array)
     suggestions_array.map do |suggestion|
-      Yelp.client.business(suggestion["food_id"])
+      new(Yelp.client.business(suggestion["food_id"]))
     end
   end
 end
+
