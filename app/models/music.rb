249d164b66c2bd38fa899fa02b78eb55e496a86e
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-18 22:10:54 -0700

    added initialize method and self.find now returns an array of Music objects instead of RSpotify objects

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,16 +1,36 @@
 require 'rspotify'
 
 class Music
+  attr_reader :name, :url, :type, :image
+
+  def initialize(data)
+    @name = data.name
+    @url = data.external_urls["spotify"]
+    @type = data.type
+    @image = image_from_data(data)
+  end
   
   def self.find(suggestion_array)
     suggestion_array.map do |suggestion|
       if suggestion["music_type"] == "artist"
-        RSpotify::Artist.find(suggestion["music_id"])
+        data = RSpotify::Artist.find(suggestion["music_id"])
       elsif suggestion["music_type"] == "album"
-        RSpotify::Album.find(suggestion["music_id"])
+        data = RSpotify::Album.find(suggestion["music_id"])
       elsif suggestion["music_type"] == "track"
-        RSpotify::Track.find(suggestion["music_id"])
+        data = RSpotify::Track.find(suggestion["music_id"])
       end
+
+      new(data)
     end
+  end
+
+  private
+
+  def image_from_data(data)
+    if data.type == "track"
+      return data.album.images.first["url"] unless data.album.images.empty?
+    end
+
+    data.images.first["url"] unless data.images.empty?
   end
 end
