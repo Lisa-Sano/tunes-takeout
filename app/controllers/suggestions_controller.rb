5b16dbbdd069d2f91b9525fb960ab258ce4e2b46
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-20 12:37:09 -0700

    added flash messages based on status codes returned from the Tunes and Takeout API for favoriting and unfavoriting items

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -13,21 +13,38 @@ class SuggestionsController < ApplicationController
     end
 
     @food, @music = get_food_and_music(@suggestions)
+    @already_favorited = TunesTakeoutWrapper.get_favorites(current_user.uid)["suggestions"]
   end
 
   def favorite
-    TunesTakeoutWrapper.favorite(current_user.uid, params[:id])
+    status_code = TunesTakeoutWrapper.favorite(current_user.uid, params[:id])
+
+    if status_code == 201
+      flash[:notice] = "Your favorite was successfully added."
+    elsif status_code == 409
+      flash[:error] = "This suggestion has already been favorited."
+    else
+      flash[:error] = "That is not a valid suggestion."
+    end
+
     redirect_to favorites_suggestions_path
   end
 
   def unfavorite
-    TunesTakeoutWrapper.unfavorite(current_user.uid, params[:id])
+    status_code = TunesTakeoutWrapper.unfavorite(current_user.uid, params[:id])
+
+    if status_code == 204
+      flash[:notice] = "You successfully removed this favorite."
+    else
+      flash[:error] = "There was an error with your request to unfavorite a suggestion."
+    end
+
     redirect_to favorites_suggestions_path
   end
 
   def favorites
-    suggestion_ids = TunesTakeoutWrapper.get_favorites(current_user.uid)["suggestions"]
-    @suggestions = get_suggestions_from_ids(suggestion_ids)
+    @already_favorited = TunesTakeoutWrapper.get_favorites(current_user.uid)["suggestions"]
+    @suggestions = get_suggestions_from_ids(@already_favorited)
 
     @food, @music = get_food_and_music(@suggestions)
   end
