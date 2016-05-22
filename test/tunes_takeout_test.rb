b4b6f62ee6f139035858efdd9b37d45b24a1baed
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-21 22:41:43 -0700

    added tests for #favorite, #unfavorite, #get_favorites methods in the TunesTakeoutWrapper

--- test/tunes_takeout_test.rb
+++ test/tunes_takeout_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 require 'tunes_takeout'
 
 describe TunesTakeoutWrapper do
+  UUID = "1bb6ba17-f330-4546-b641-6772af537587"
+
   describe "search" do
     before do
       @search = TunesTakeoutWrapper.search("banana")
@@ -59,6 +61,10 @@ describe TunesTakeoutWrapper do
       @retrieved = TunesTakeoutWrapper.retrieve("Vz0KNY-RRwADbn2k")
     end
 
+    it "returns nil if no suggestion with that id exists", :vcr do
+      assert_nil TunesTakeoutWrapper.retrieve(12345)
+    end
+
     it "returns a Hash", :vcr do
       assert_instance_of Hash, @retrieved
     end
@@ -80,9 +86,64 @@ describe TunesTakeoutWrapper do
     end
   end
 
-  describe "favorite" do
+  describe "favorite with real suggestion id" do
+
     before do
-      @favorite = TunesTakeoutWrapper.favorite('')
+      @favorite = TunesTakeoutWrapper.favorite(UUID, "Vz0KNY-RRwADbn2k")
+    end
+
+    after do
+      TunesTakeoutWrapper.unfavorite(UUID, "Vz0KNY-RRwADbn2k")
+    end
+
+    it "should return status code 201 if successful", :vcr do
+      assert_equal 201, @favorite
+    end
+
+    it "should return a status code 409 if the user already favorited that suggestion", :vcr do
+      favorite_again = TunesTakeoutWrapper.favorite(UUID, "Vz0KNY-RRwADbn2k")
+      assert_equal 409, favorite_again
+    end
+
+    describe "get_favorites" do
+      before do
+        @get_favorites = TunesTakeoutWrapper.get_favorites(UUID)
+      end
+
+      it "should return a Hash", :vcr do
+        assert_instance_of Hash, @get_favorites
+      end
+
+      it "should contain the favorited suggestion Vz0KNY-RRwADbn2k", :vcr do
+        assert_equal true, @get_favorites["suggestions"].include?("Vz0KNY-RRwADbn2k")
+      end
+    end
+  end
+
+  describe "favorite with non-existent suggestion id" do
+
+    it "should return a status code 404 if no suggestion with the id exists", :vcr do
+      status = TunesTakeoutWrapper.favorite(UUID, 12345)
+      assert_equal 404, status
+    end
+  end
+
+  describe "unfavorite" do
+    before do
+      TunesTakeoutWrapper.favorite(UUID, "Vz0KNY-RRwADbn2k")
+      @unfavorite = TunesTakeoutWrapper.unfavorite(UUID, "Vz0KNY-RRwADbn2k")
+    end
+
+    it "should remove a previously favorited suggestion from the user's favorite list", :vcr do
+      assert_equal false, TunesTakeoutWrapper.get_favorites(UUID).include?("Vz0KNY-RRwADbn2k")
+    end
+
+    it "should return a status code 204 if successful", :vcr do
+      assert_equal 204, @unfavorite
+    end
+
+    it "should return a status code 404 if the suggestion id is fake", :vcr do
+      assert_equal 404, TunesTakeoutWrapper.unfavorite(UUID, 12345)
     end
   end
 end
