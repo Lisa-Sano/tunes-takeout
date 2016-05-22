db6c1cffc2844ec553d0e596af849bdc4999ac9a
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-21 20:55:54 -0700

    added tests for TunesTakeoutWrapper

--- test/tunes_takeout_test.rb
+++ test/tunes_takeout_test.rb
@@ -7,12 +7,28 @@ describe TunesTakeoutWrapper do
       @search = TunesTakeoutWrapper.search("banana")
     end
 
-    it "returns a hash", :vcr do
+    it "returns a Hash", :vcr do
       assert_instance_of Hash, @search
+    end
+
+    it "returns a Hash with 2 keys", :vcr do
+      assert_equal 2, @search.length
+    end
+
+    it "returns a Hash with an href key", :vcr do
+      refute_nil @search["href"]
+    end
+
+    it "returns a Hash with a suggestions key", :vcr do
+      refute_nil @search["suggestions"]
     end
 
     it "returns 10 suggestions by default", :vcr do
       assert_equal 10, @search["suggestions"].length
+    end
+
+    it "returns an id for each suggestion", :vcr do
+      refute_nil @search["suggestions"][0]["id"]
     end
 
     it "returns a food_id for each suggestion", :vcr do
@@ -35,6 +51,38 @@ describe TunesTakeoutWrapper do
 
     it "returns 20 suggestions by default", :vcr do
       assert_equal 20, @top["suggestions"].length
+    end
+  end
+
+  describe "retrieve" do
+    before do
+      @retrieved = TunesTakeoutWrapper.retrieve("Vz0KNY-RRwADbn2k")
+    end
+
+    it "returns a Hash", :vcr do
+      assert_instance_of Hash, @retrieved
+    end
+
+    it "returns a Hash with 2 keys", :vcr do
+      assert_equal 2, @retrieved.length
+    end
+
+    it "returns a Hash with an href key", :vcr do
+      refute_nil @retrieved["href"]
+    end
+
+    it "returns a Hash with a suggestion key", :vcr do
+      refute_nil @retrieved["suggestion"]
+    end
+
+    it "returns a suggestion in a Hash", :vcr do
+      assert_instance_of Hash, @retrieved["suggestion"]
+    end
+  end
+
+  describe "favorite" do
+    before do
+      @favorite = TunesTakeoutWrapper.favorite('')
     end
   end
 end
