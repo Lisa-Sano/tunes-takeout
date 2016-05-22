5041ee624945545a646cd578ff875fde495a636f
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-21 20:55:35 -0700

    test for oauth users using OmniAuth mock_auth config

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  def setup
+    # @known = { "provider" => 'spotify', "info" => { "id" => "known_user", "name" => "known user", "image" => "image"} }
+    @known = OmniAuth.config.mock_auth[:spotify_known]
+    @unknown = OmniAuth.config.mock_auth[:spotify_unknown]
+    # @unknown_with_uid = OmniAuth.config.mock_auth[:spotify_uid]
+  end
+
+  test "can find an existing user given an oauth spotify hash" do
+    assert_equal users(:known_user), User.find_or_create_from_omniauth(@known)
+  end
+
+  test "can make a new user given the oauth spotify hash of an unknown user" do
+    assert_difference 'User.count', 1 do
+      @user = User.find_or_create_from_omniauth @unknown
+    end
+  end
+
+  test "uses oauth data to set user name, provider, image and uid for new users" do
+    user = User.find_or_create_from_omniauth @unknown
+
+    assert_equal @unknown['info']['name'], user.name
+    assert_equal @unknown['provider'], user.provider
+    assert_equal @unknown['uid'], user.uid
+    assert_equal @unknown['info']['image'], user.image
+  end
+
+  # test "prefers a top-level uid over a nested id when creating an oauth user" do
+  #   user = User.find_or_create_from_omniauth @unknown_with_uid
+  #   assert_equal @unknown_with_uid['uid'], user.uid
   # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,5 +25,16 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:spotify_known] = OmniAuth::AuthHash.new(
+       { provider: "spotify", uid: "known_user", info: { "name" => "known user", "image" => "image" }
+  })
+
+  OmniAuth.config.mock_auth[:spotify_unknown] = OmniAuth::AuthHash.new(
+       { provider: "spotify", uid: "lisa", info: { "name" => "lisa", "image" => "lisa_image" }
+  })
+
+  # OmniAuth.config.mock_auth[:spotify_uid] = OmniAuth::AuthHash.new(
+  #      { provider: "spotify", uid: "preferred", info: { "id" => "not_lisa", "name" => "lisa", "image" => "not_lisa_image" }
+  # })
 end
